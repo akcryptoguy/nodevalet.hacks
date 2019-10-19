bd90781285fc126a25906a26bcac5d5bfefac6c0
AKcryptoGUY <45030509+akcryptoguy@users.noreply.github.com>
2019-10-19 17:07:01 +0000

    Scriplet to edit maintenance logs

---
+++ mlogedit.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+INSTALLDIR='/var/tmp/nodevalet'
+INFODIR='/var/tmp/nvtemp'
+PROJECT=`cat $INFODIR/vpscoin.info`
+MNS=`cat $INFODIR/vpsnumber.info`
+LOGFILE='/var/tmp/nodevalet/logs/maintenance.log'
+
+nano $LOGFILE
+
+echo -e "\n"
+echo -e "`date +%m.%d.%Y_%H:%M:%S` : mlogedit.sh was run to modify the Maintenance Log." | tee -a "$LOGFILE"
+  
+# echo -e " Your edits to the Maintenance Log have been logged. \n"
