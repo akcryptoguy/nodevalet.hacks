4484d09e11196dbbcafa6cbfb65850a73ac26f1d
AKcryptoGUY <45030509+akcryptoguy@users.noreply.github.com>
2019-11-01 00:44:12 +0000

    Update mlogedit.sh

--- mlogedit.sh
+++ mlogedit.sh
@@ -13,6 +13,6 @@ HNAME=$(<$INFODIR/vpshostname.info)
 nano -c $LOGFILE
 
 echo -e "\n"
-echo -e "`date +%m.%d.%Y_%H:%M:%S` : mlogedit.sh was run to modify the Maintenance Log.\n" | tee -a "$LOGFILE"
+echo -e " $(date +%m.%d.%Y_%H:%M:%S) : mlogedit.sh was run to modify the Maintenance Log.\n" | tee -a "$LOGFILE"
 
 echo -e " Your edits to the Maintenance Log have been logged. \n"
